c4568cf48c986cd71643b82bf54d5bce97d57ed8
David Doherty <froztydavie@Davids-MacBook-Air-5.local>
2025-04-23 23:01:13 -0700

    added inital controls

---
+++ src/components/Controls.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Controls(){
+    return (
+        <div className="controls">
+            <button className="control-button" onClick={(e) => {}}>Left</button>
+            <button className="control-button" onClick={(e) => {}}>Right</button>
+            <button className="control-button" onClick={(e) => {}}>Down</button>
+            <button className="control-button" onClick={(e) => {}}>Rotate</button>
+        </div>
+    );
+}
