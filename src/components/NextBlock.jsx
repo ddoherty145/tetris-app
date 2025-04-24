f3b3f15c55296ec24b4dfe048cc04201f6f8d8a6
David Doherty <froztydavie@Davids-MacBook-Air-5.local>
2025-04-24 00:38:58 -0700

    added connection for next block

--- src/components/NextBlock.jsx
+++ src/components/NextBlock.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import GridSquare from './GridSquare';
+import { useSelector } from 'react-redux';
+import { shapes } from '../utils';
 
 export default function NextBlock() {
-    const block = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-    ];
+  const nextShape = useSelector((state) => state.game.nextShape);
+  const block = shapes[nextShape][0];
 
-    const grid = block.map((rowArray, row) =>
-        rowArray.map((square, col) => (
-            <GridSquare key={`${row}${col}`} color={square} />
-        ))
-    );
+  const grid = block.map((rowArray, row) =>
+    rowArray.map((square, col) => {
+      const color = square ? nextShape : 0;
+      return <GridSquare key={`${row}${col}`} color={color} />;
+    })
+  );
 
-    return <div className="next-block">{grid}</div>
-
+  return <div className="next-block">{grid}</div>;
 }
