a5c0a694e75970b944328b84b2585686d1fe454e
David Doherty <froztydavie@Davids-MacBook-Air-5.local>
2025-04-26 13:11:46 -0700

    connection from message popup

--- src/components/NextBlock.jsx
+++ src/components/NextBlock.jsx
@@ -1,39 +1,21 @@
-// import React from 'react';
-// import GridSquare from './GridSquare';
-// import { useSelector } from 'react-redux';
-// import { shapes } from '../utils';
-
-// export default function NextBlock() {
-//   const nextShape = useSelector((state) => state.nextShape);
-//   const block = shapes[nextShape][0];
-
-//   const grid = block.map((rowArray, row) =>
-//     rowArray.map((square, col) => {
-//       const color = square ? nextShape : 0;
-//       return <GridSquare key={`${row}${col}`} color={color} />;
-//     })
-//   );
-
-//   return <div className="next-block">{grid}</div>;
-// }
-
 import React from 'react';
 import GridSquare from './GridSquare';
+import { useSelector } from 'react-redux';
+import { shapes } from '../utils';
 
-// Draws the "next" block view showing the next block to drop
 export default function NextBlock() {
-  const block = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-  ];
+  const nextShape = useSelector((state) => state.game.nextShape);
+  const shapeArray = shapes[nextShape] ? shapes[nextShape][0] : shapes[0][0];
 
-  const grid = block.map((rowArray, row) =>
-    rowArray.map((square, col) => (
-      <GridSquare key={`${row}${col}`} color={square} />
-    ))
-  );
+  const grid = [];
+
+  for (let row = 0; row < 4; row++) {
+    for (let col = 0; col < 4; col++) {
+      const isFilled = shapeArray[row] && shapeArray[row][col];
+      const color = isFilled ? nextShape : 0;
+      grid.push(<GridSquare key={`${row}${col}`} color={color} />);
+    }
+  }
 
   return <div className="next-block">{grid}</div>;
 }
--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -5,10 +5,10 @@ export default function ScoreBoard() {
         <div className ="score-board">
             <div>Score: 0</div>
             <div>Levels: 1</div>
-            <button className="score-board-button" onClick={(e) => {
+            <button className="score-board-button" onClick={() => {
                 //...
             }}>Play</button>
-            <button className="score-board-button" onClick={(e) => {
+            <button className="score-board-button" onClick={() => {
                 //...
             }}>Restart</button>
         </div>
